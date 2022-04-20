392dbb65c14a64c3c1d83be7ac96cc8edc7c6332
Subrahmanaya <97424360+Subrahmanaya@users.noreply.github.com>
2022-04-20 10:00:05 +0530

    #include<stdio.h> int main() {     int n,r,s=0;     scanf("%d",&n);     while(n!=1&&n!=4)     {         s=0;         while(n>0)         {             r=n%10;             s=s+(r*r);             n=n/10;         }         n=s;     }     if(s==1||s==7)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ happy_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,s=0;
+    scanf("%d",&n);
+    while(n!=1&&n!=4)
+    {
+        s=0;
+        while(n>0)
+        {
+            r=n%10;
+            s=s+(r*r);
+            n=n/10;
+        }
+        n=s;
+    }
+    if(s==1||s==7)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
