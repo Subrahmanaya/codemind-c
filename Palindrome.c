0bb245ccd730af3871d9d69584ae573e860c924f
Subrahmanaya <97424360+Subrahmanaya@users.noreply.github.com>
2022-05-17 22:20:01 +0530

    #include<stdio.h> int main() { 	int d,n,num,rev=0; 	scanf("%d",&n); 	num=n; 	while(n) 	{ 		d=n%10; 		n=n/10; 		rev=rev*10+d; 	} 	if(num==rev) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	} }

---
+++ Palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int d,n,num,rev=0;
+	scanf("%d",&n);
+	num=n;
+	while(n)
+	{
+		d=n%10;
+		n=n/10;
+		rev=rev*10+d;
+	}
+	if(num==rev)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+}
