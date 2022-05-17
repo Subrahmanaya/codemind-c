28fe9e41929f38fb516694fce4431256917f640c
Subrahmanaya <97424360+Subrahmanaya@users.noreply.github.com>
2022-05-17 22:28:52 +0530

    #include<stdio.h> #include<math.h> int main() { 	int m,p,i,j=0,n,r,num,rev=0; 	scanf("%d",&n); 	m=n*n; 	while(n>0) 	{ 		r=n%10; 		rev=(rev*10)+r; 		n=n/10; 	} 	p=rev*rev; 	while(p>0) 	{ 	    i=p%10; 	    j=(j*10)+i; 	    p=p/10; 	} 	if(j==m) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	} }

---
+++ Adam_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int m,p,i,j=0,n,r,num,rev=0;
+	scanf("%d",&n);
+	m=n*n;
+	while(n>0)
+	{
+		r=n%10;
+		rev=(rev*10)+r;
+		n=n/10;
+	}
+	p=rev*rev;
+	while(p>0)
+	{
+	    i=p%10;
+	    j=(j*10)+i;
+	    p=p/10;
+	}
+	if(j==m)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+}
